db7f8e82798a78426a40181c6ce799749a15de12
Nicholas de Jong <contact@nicholasdejong.com>
2019-03-16 14:20:44 +0800

    migrating FauxAPI Pythin lib to a tested, built module

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+from PfsenseFauxapi.__version__ import __version__
+
+setup(
+    name='PfsenseFauxapi',
+    version=__version__,
+    description='Python client for the pfSense-FauxAPI on a pfSense host',
+
+    classifiers=[
+        'Topic :: System :: Networking :: Firewalls',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+    keywords='pfsense pfsense-fauxapi devops',
+
+    author='Nicholas de Jong',
+    author_email='contact@nicholasdejong.com',
+    url='https://github.com/ndejong/pfsense_fauxapi',
+    license='Apache',
+
+    packages=find_packages(),
+    scripts=['bin/fauxapipy'],
+    install_requires=['requests'],
+
+)
---
+++ tests/test_PfsenseFauxapi.py
@@ -0,0 +1,9 @@
+
+import time
+import pytest
+from PfsenseFauxapi.PfsenseFauxapi import PfsenseFauxapi
+
+
+def test_version_exist():
+    fauxapi = PfsenseFauxapi(host=None, apikey=None, apisecret=None)
+    assert fauxapi.version is not None
