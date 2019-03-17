8f0a25184c354917bc3f612f3be913545a9d036d
Nicholas de Jong <contact@nicholasdejong.com>
2019-03-17 12:35:59 +0800

    adds a long_description for PyPi

--- setup.py
+++ setup.py
@@ -3,10 +3,16 @@
 from setuptools import setup, find_packages
 from PfsenseFauxapi.__version__ import __version__
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(
     name='pfsense-fauxapi',
     version=__version__,
     description='Python client for the pfSense-FauxAPI on a pfSense host',
+
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 
     classifiers=[
         'Topic :: System :: Networking :: Firewalls',
